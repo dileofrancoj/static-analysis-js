e524befd9bfc886b8fc919f4ee0c256ba398372a
Franco Di Leo <dileo.francoj@gmail.com>
2022-04-21 14:25:34 -0300

    create: init repo

---
+++ src/example.js
@@ -0,0 +1,9 @@
+const user = "fran";
+typeof user === "string";
+
+if(!('serviceWorker' in navigator)) {
+    // old browser bro
+}
+
+const greetingPeya = "hi peya";
+console.log(`${greetingPeya} viewers`);
