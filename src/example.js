26a6106233e7422a0a4dc3e9f4c04a4338d24997
Franco Di Leo <dileo.francoj@gmail.com>
2022-04-25 11:54:46 -0300

    feat: add prettier and eslint-config-prettier

--- src/example.js
+++ src/example.js
@@ -1,9 +1,9 @@
-const user = "fran";
-typeof user === "string";
+const user = 'fran'
+typeof user === 'string'
 
-if (!("serviceWorker" in navigator)) {
+if (!('serviceWorker' in navigator)) {
   // old browser bro
-}
+};
 
-const greetingPeya = "hi peya";
-console.log(`${greetingPeya} viewers`);
+const greetingPeya = 'hi peya'
+console.log(`${greetingPeya} viewers`)
